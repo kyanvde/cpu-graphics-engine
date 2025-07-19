f31d9af55db044f8500fa2b5ed0c9a8c1e3bbf50
Kyan Van den Eynde <kyanvde@gmail.com>
2025-07-19 12:19:01 +0200

    feat(math): add vec3

---
+++ src/math/Vec3.h
@@ -0,0 +1,36 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <cmath>
+
+struct Vec3 {
+    float x, y, z;
+
+    Vec3(float x = 0, float y = 0, float z = 0) : x(x), y(y), z(z) {}
+
+    Vec3 operator+(const Vec3& other) const {
+        return {x + other.x, y + other.y, z + other.z};
+    }
+
+    Vec3 operator*(const float scalar) const {
+        return {x * scalar, y * scalar, z * scalar};
+    }
+
+    Vec3 operator*(const Vec3& other) const {
+        return {x * other.x, y * other.y, z * other.z};
+    }
+
+    Vec3 cross(const Vec3& other) const {
+        return {
+            y * other.z - z * other.y,
+            z * other.x - x * other.z,
+            x * other.y - y * other.x
+        };
+    }
+
+    Vec3 normalized() const {
+        return std::sqrt(x*x + y*y + z*z);
+    }
+};
+
+#endif // VEC3_H
