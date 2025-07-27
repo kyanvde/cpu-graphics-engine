48afdbfc6fcff4155eb19223882ce32c278781d2
Kyan Van den Eynde <kyanvde@gmail.com>
2025-07-27 20:16:36 +0200

    style(src): Reformat engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -6,62 +6,67 @@
 #include <string>
 #include <vector>
 
-#include "io/input/SceneParser.h"
-#include "io/input/parsers/JsonSceneParser.h"
-#include "io/output/writers/BMPWriter.h"
+#include "io/parsers/JsonSceneParser.h"
+#include "io/parsers/SceneParser.h"
+#include "io/writers/BMPWriter.h"
 #include "model/Image.h"
 
 using ParserFactory = std::function<std::unique_ptr<SceneParser>()>;
 
-std::unique_ptr<SceneParser> createParserForSceneFile(const std::string& sceneFile) {
-    const std::string extension = std::filesystem::path(sceneFile).extension().string();
+std::unique_ptr<SceneParser> createParserForSceneFile(
+    const std::string& sceneFile) {
+  const std::string extension =
+      std::filesystem::path(sceneFile).extension().string();
 
-    if (extension == ".json") return std::make_unique<JsonSceneParser>();
+  if (extension == ".json") {
+    return std::make_unique<JsonSceneParser>();
+  }
 
-    std::cerr << "Unsupported extension: " << extension << std::endl;
-    return nullptr;
+  throw std::runtime_error("Unsupported sceneFile extension: " + extension);
 }
 
 int main(int argc, char* argv[]) {
-    try {
-        std::vector<std::string> args(argv + 1, argv + argc);
+  try {
+    std::vector<std::string> args(argv + 1, argv + argc);
 
-        // If no filenames are provided in the program arguments
-        if (args.empty()) {
-            std::ifstream fileIn("filelist");
-            if (!fileIn) {
-                std::cerr << "Error: Could not open 'filelist'" << std::endl;
-                return EXIT_FAILURE;
-            }
-
-            std::string filelistName;
-            while (std::getline(fileIn, filelistName)) {
-                if (!filelistName.empty()) {
-                    args.push_back(filelistName);
-                }
-            }
-        }
-
-        // Render each scene
-        for (const std::string& sceneFile : args) {
-            std::unique_ptr<SceneParser> parser = createParserForSceneFile(sceneFile);
-            if (!parser) {
-                std::cerr << "Skipping file: " << sceneFile << std::endl;
-                continue;
-            }
-
-            Scene scene = parser->parse(sceneFile);
-            Image image = scene.render();
-            std::string outputFileName = sceneFile.substr(0, sceneFile.find_last_of('.')) + ".bmp";
-            if (BMPWriter writer; !image.writeTo(outputFileName, writer)) {
-                std::cerr << "Error writing to file: " << outputFileName << std::endl;
-            }
-        }
-
-        return EXIT_SUCCESS;
-
-    } catch (const std::bad_alloc&) {
-        std::cerr << "Error: insufficient memory" << std::endl;
+    // If no filenames are provided in the program arguments
+    if (args.empty()) {
+      std::ifstream fileIn("filelist");
+      if (!fileIn) {
+        std::cerr << "Error: Could not open 'filelist'" << '\n';
         return EXIT_FAILURE;
+      }
+
+      std::string filelistName;
+      while (std::getline(fileIn, filelistName)) {
+        if (!filelistName.empty()) {
+          args.push_back(filelistName);
+        }
+      }
     }
+
+    // Render each scene
+    for (const std::string& sceneFile : args) {
+      try {
+        std::unique_ptr<SceneParser> parser =
+            createParserForSceneFile(sceneFile);
+
+        Scene scene = parser->parse(sceneFile);
+        Image image = scene.render();
+        std::string outputFileName =
+            sceneFile.substr(0, sceneFile.find_last_of('.')) + ".bmp";
+        if (BMPWriter writer; !image.writeTo(outputFileName, writer)) {
+          throw std::runtime_error("Error writing to file: " + outputFileName);
+        }
+      } catch (const std::exception& e) {
+        std::cerr << "Failed to render scene (" << sceneFile
+                  << "): " << e.what() << '\n';
+      }
+    }
+
+    return EXIT_SUCCESS;
+  } catch (const std::bad_alloc&) {
+    std::cerr << "Error: insufficient memory" << '\n';
+    return EXIT_FAILURE;
+  }
 }
