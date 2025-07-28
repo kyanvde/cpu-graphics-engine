98f3719c5f2017a6685c2f1472999a093cce1334
Kyan Van den Eynde <kyanvde@gmail.com>
2025-07-28 15:48:22 +0200

    test(Projector): Add unit tests for projectPoint method

--- src/math/Projector.cpp
+++ src/math/Projector.cpp
@@ -1,6 +1,5 @@
 #include "Projector.h"
 
-#include <iostream>
 Vec3 Projector::projectPoint(const Vec3& point, double focalLength) {
   double xCoord = focalLength * point.x / -point.z;
   double yCoord = focalLength * point.y / -point.z;
