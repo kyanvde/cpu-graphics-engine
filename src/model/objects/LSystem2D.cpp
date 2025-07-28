6a6da3a48c925783c2fb0f9fa54468bba6306ad5
Kyan Van den Eynde <kyanvde@gmail.com>
2025-07-28 13:49:14 +0200

    feat(model): Add access methods to LSystem2D

--- src/model/objects/LSystem2D.cpp
+++ src/model/objects/LSystem2D.cpp
@@ -1,1 +1,36 @@
 #include "LSystem2D.h"
+
+#include <stdexcept>
+
+std::vector<Line> LSystem2D::getProjectedLines() {}
+
+void LSystem2D::render(Image& image) {}
+
+void LSystem2D::addSymbol(const char symbol) {
+  this->alphabet.insert({symbol, false});
+}
+
+void LSystem2D::setDrawSymbol(const char symbol, const bool value) {
+  const auto iterator = this->alphabet.find(symbol);
+  if (iterator == this->alphabet.end()) {
+    throw std::runtime_error("Attempted to set draw value for unknown symbol");
+  }
+  iterator->second = value;
+}
+
+void LSystem2D::addReplacementRule(char from, const std::string& replacement) {
+  this->replacement.insert({from, replacement});
+}
+
+void LSystem2D::setInitiator(const std::string& initiator) {
+  this->initiator = initiator;
+}
+
+void LSystem2D::setAngle(const double angle) { this->angle = angle; }
+
+void LSystem2D::setStartingAngle(const double angle) {
+  this->startingAngle = angle;
+}
+void LSystem2D::setIterations(const unsigned int iterations) {
+  this->iterations = iterations;
+}
--- src/model/objects/LSystem2D.h
+++ src/model/objects/LSystem2D.h
@@ -2,24 +2,30 @@
 #define LSYSTEM2D_H
 
 #include <map>
-#include <set>
 #include <string>
 #include <vector>
 
 #include "model/SceneObject.h"
 
 class LSystem2D final : public SceneObject {
-  std::set<char> alphabet;
-  std::map<char, bool> draw;
-  std::map<char, std::string> replacement;
+  std::map<const char, bool> alphabet;
+  std::map<const char, std::string> replacement;
   std::string initiator;
-  double angle = 90;
+  double angle = 0;
   double startingAngle = 0;
   unsigned int iterations = 1;
 
  public:
   std::vector<Line> getProjectedLines() override;
   void render(Image& image) override;
+
+  void addSymbol(char symbol);
+  void setDrawSymbol(char symbol, bool value);
+  void addReplacementRule(char from, const std::string& replacement);
+  void setInitiator(const std::string& initiator);
+  void setAngle(double angle);
+  void setStartingAngle(double angle);
+  void setIterations(unsigned int iterations);
 };
 
 #endif  // LSYSTEM2D_H
